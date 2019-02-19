0fa1feb32eb659abe89a239681e6d3aff416248b
Suraj Rajendran <surajemail@yahoo.com>
2019-02-19 06:59:27 +0000

    Create functional_programming.cmd

---
+++ functional_programming.cmd
@@ -0,0 +1,74 @@
+PRG - FUNCTIONAL(eg., LAMBDA),PARALLEL(eg. map with Async (eg., stream) ), easy functional TESTABLE(lambda(i/p parameter of fn_interfce(lambda) is another function))
+-------------------------------------------------------------------------------------------------------------------------------------
+
+Dont iterate over list ..use mp and reduce (put all data in memory & parallel and process fast ) ..use lambda  for fn prog. Of result
+
+Color =  list.stream().map(lambda colorResults:compare_FR_To_FC within Settlment_Date(f1,f2...),(redstatus,greenstatus,amberstatus))
+
+colorResults=(redfalse,greentrue,amberfalse)
+
+example:
+This is a map that squares every number in the passed collection:
+
+squares = map(lambda x: x * x, [0, 1, 2, 3, 4])
+
+print squares
+# => [0, 1, 4, 9, 16]
+
+
+
+1.compar and updayefile bersion..get green flag
+2. Compate versioned file to get amber flag agin
+
+ColorResult= list.st­ream­().map(lambda colorR­esults:compare­_FR_To­_FC within Se­ttlment­_Date(....),(red­status,gree­nstatus,a­mberstatus­))
+colorResults=(redfal­­se,greentrue,amberf­a­lse)
+
+compare­_FR_To­_FC within Se­ttlment­_Date=list.st­ream­().map(lambda precom( f1(p1(m1,m3­),p2(m7)),f3(.....) ),(filereceived1,....))
+,
+example:
+This is a map that squares every number in the passed collec­­tion:
+squares = map(lambda x: x * x, [0, 1, 2, 3, 4])
+print squares
+# => [0, 1, 4, 9, 16]
+
+
+
+
+ColorResult= list.st­­ream­().
+map(lambda x: x * x, [filereceived1,....])
+map(lambda colorR­es­ults:compare­_FR_To­­_FC within (f1...))
+colorResults=(redfa­l­­se,greentrue,ambe­rf­a­lse)
+
+
+
+precom(  f1(p1(m1,m3),p2(m7)),f3(.....)  )
+
+ColorResult= list.st­ream­().
+
+map(lambda x: x * x, [0, 1, 2, 3, 4])
+
+map(lambda colorR­esults:compare­_FR_To­_FC within (f1...))
+colorResults=(redfal­­se,greentrue,amberf­a­lse)
+
+
+
+ColorResult= list.stream­().map(lambda colorR­esults:compare_FR_To­_FC within Settlment­_Date(f1,f2...),(red­status,greenstatus,a­mberstatus))
+
+colorResults=(redfal­se,greentrue,amberfa­lse)
+
+example:
+
+This is a map that squares every number in the passed collec­tion:
+
+squares = map(lambda x: x * x, [0, 1, 2, 3, 4])
+
+print squares
+
+# => [0, 1, 4, 9, 16]
+
+
+
+https://read.acloud.guru/the-need-for-asynchronous-rpc-architecture-in-serverless-systems-ff168f1c8785
+
+
+
